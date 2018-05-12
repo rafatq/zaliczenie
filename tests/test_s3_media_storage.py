e031c1daed253f3c3a451ba43cc189f4c79c1d30
Kuba Kanclerz <kanclerj@uek.krakow.pl>
2018-05-12 08:17:38 +0000

    ensure file exists

--- tests/test_s3_media_storage.py
+++ tests/test_s3_media_storage.py
@@ -22,4 +22,11 @@ class TestS3Storage(unittest.TestCase):
     return S3MediaStorage(s3, bucket_name)
   
   def there_is_file(self):
-    pass
+    my_file = open('/tmp/test.txt', 'w')
+    my_file.write('My test content :D')
+    my_file.close()
+    
+    return open('/tmp/test.txt', 'rb')
+
+
+
